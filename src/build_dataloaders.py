0f5629e4500dfa41b350a6db086b0b072e5c379b
BluOyster29 <rthomas2907@gmail.com>
2024-03-07 18:12:02 +0000

    building dataloader

---
+++ src/build_dataloaders.py
@@ -0,0 +1,117 @@
+from collections import Counter
+import torch
+from torch.utils.data import WeightedRandomSampler, DataLoader
+from transformers import BertTokenizer
+
+from persuasion_strategy_dataset import PersuasionStrategyDataset
+from utils import load_config, get_args, output_pickle, gen_metadata
+from preprocess import import_data
+
+
+def build_sampler(training_dataset):
+    """
+    Build a weighted random sampler to balance the dataset during training.
+
+    Args:
+        training_dataset (Dataset): The training dataset.
+
+    Returns:
+        WeightedRandomSampler: The weighted random sampler.
+
+    """
+    labels = [torch.argmax(label['labels']).item() for label in training_dataset]  # Assuming your dataset returns (input_vector, label) pairs
+    class_distribution = Counter(labels)
+
+    # Calculate weights for each sample
+    class_weights = {class_label: len(training_dataset) / (len(class_distribution) * class_count) for class_label, class_count in class_distribution.items()}
+    weights = [class_weights[label] for label in labels]
+
+    # Convert weights to tensor
+    weights_tensor = torch.tensor(weights, dtype=torch.float)
+
+    # Create a sampler to balance the dataset during training
+    sampler = WeightedRandomSampler(weights_tensor, len(weights_tensor))
+    return sampler
+
+def gen_dataloader(df, tokenizer, max_token_len, batch_size, sampler=None):
+    """
+    Generate a DataLoader object from a DataFrame.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        tokenizer (BertTokenizer): The tokenizer to use for encoding the data.
+        max_token_len (int): The maximum token length for the input data.
+        batch_size (int): The batch size for the DataLoader.
+        sampler (WeightedRandomSampler): The sampler to use for the DataLoader.
+
+    Returns:
+        DataLoader: The DataLoader object.
+
+    """
+    dataset = PersuasionStrategyDataset(
+        data=df,
+        tokenizer=tokenizer,
+        max_token_len=max_token_len
+    )
+
+    if sampler:
+        sampler = build_sampler(dataset)
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            sampler=sampler
+        )
+    else:
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size
+        )
+
+    return dataloader
+
+def build_dataloaders(train_df, test_df, config):
+    """
+    Build dataloaders for training and testing data.
+
+    Args:
+        train_df (pandas.DataFrame): The training dataset.
+        test_df (pandas.DataFrame): The testing dataset.
+        config (dict): A dictionary containing configuration parameters.
+
+    Returns:
+        tuple: A tuple containing the training dataloader and testing dataloader.
+    """
+    tokenizer = BertTokenizer.from_pretrained(config.get('pretrained_model'))
+
+    train_dataloader = gen_dataloader(
+        train_df,
+        tokenizer,
+        config.get('max_token_len'),
+        config.get('batch_size'),
+        config.get('sampler')
+    )
+
+    if config.get('output_dataloader'):
+        metadata = gen_metadata(config, 'dataloader')
+        output_pickle(train_dataloader, config, 'training_dataloader_{}'.format(metadata) + '.pkl')
+        
+    test_dataloader = gen_dataloader(
+        test_df,
+        tokenizer,
+        config.get('max_token_len'),
+        config.get('batch_size')
+    )
+
+    if config.get('output_dataloader'):
+        metadata = gen_metadata(config, 'dataloader')
+        output_pickle(test_dataloader, config, 'testing_dataloader_{}'.format(metadata) + '.pkl')
+    
+    return train_dataloader, test_dataloader
+
+if __name__ == '__main__':
+    # Example usage
+    args = get_args()
+    config = load_config(args.config)
+    train_df, test_df, label_names = import_data(config)
+    training_dataloader, testing_dataloader = build_dataloaders(train_df, test_df, config)
+    

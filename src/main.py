49b560e36c2dc53a6c48ca2a3398cb46a528ea54
BluOyster29 <rthomas2907@gmail.com>
2024-03-07 18:12:23 +0000

    main script

--- src/main.py
+++ src/main.py
@@ -1,29 +1,27 @@
-from utils import getargs, load_config
-from preprocess import preprocess_data
+import logging 
+from preprocess import import_data
+from build_dataloaders import build_dataloaders
+from train import train
+from utils import get_args, load_config
+import warnings 
 
+warnings.filterwarnings("ignore", category=FutureWarning) 
+# until i work out pandas bug 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-
-
-
-def main():
-    args = getargs()
-    config = load_config(args.config)
-    training_dataloader, testing_dataloader = preprocess_data(
-        config)
-
-    output(
-        training_dataloader,
-        f'{config.get("dataloader_path")}training_dataloader.pkl'
-    )
-
-    output(
-        testing_dataloader,
-        f'{config.get("data_loader_path")}test_dataloader.pkl'
-    )
-
-    return
-
+def main(parsed_args):
+    
+    logger.info('Loading config from %s', parsed_args.config_path)
+    config = load_config(parsed_args.config_path)
+    logger.info('Config Loaded')
+    logger.info('Importing Data')
+    train_df, test_df, label_columns = import_data(config)
+    training_dataloader, testing_dataloader = build_dataloaders(train_df[:1], test_df[:1], config)
+    trained_model = train(config, label_columns, training_dataloader)
 
 if __name__ == '__main__':
-    main()
+    logger.info('Starting Main')
+    args = get_args()
+    main(args)

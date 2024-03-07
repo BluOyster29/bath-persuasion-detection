89ac9ffd98840ab492c6dc22312d08d991a2d4a7
BluOyster29 <rthomas2907@gmail.com>
2024-03-07 18:13:02 +0000

    preprocessing script

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,122 +1,72 @@
 import pandas as pd
-import torch
-import pickle as pkl
-from datetime import datetime
-
-from torch.utils.data import DataLoader
-from collections import Counter
-
 from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer
-from torch.utils.data import WeightedRandomSampler
-from persuasionDataset import PersuasionDataset
-from utils import load_config, getargs
-import warnings
 
-# Ignore specific warning types
-warnings.filterwarnings("ignore", category=FutureWarning)
+def load_csvs(path_to_training, path_to_testing, concat, test_size):
+    
+    if concat:
+        train_df, test_df = concat_df(path_to_training, path_to_testing, test_size)
+    else:
+        train_df = pd.read_csv(path_to_training)
+        test_df = pd.read_csv(path_to_testing)
+    
+    return train_df, test_df
 
-def output(ob, path):
-    path = path + datetime.now().strftime("%Y%m%d%H%M%S") + '.pkl'
-    with open(path, 'wb') as file:
-        pkl.dump(ob, file)
+def concat_df(df1_path: str, df2_path: str, test_size: float) -> tuple:
+    """
+    Concatenates two dataframes and splits the combined dataframe into train and test sets.
 
+    Parameters:
+    df1_path (str): The file path of the first dataframe.
+    df2_path (str): The file path of the second dataframe.
+    test_size (float): The proportion of the dataframe to include in the test set.
 
-def load_data(config: dict):
+    Returns:
+    tuple: A tuple containing four dataframes: X_train, X_test, y_train, y_test.
+    """
 
-    df = pd.concat([
-        pd.read_csv(f"{config.get('data_root_path')}training.csv"),
-        pd.read_csv(f"{config.get('data_root_path')}testing.csv")]
-    )
+    df = pd.concat([pd.read_csv(df1_path), pd.read_csv(df2_path)])
+    return train_test_split(
+        df, test_size=test_size, shuffle=True, random_state=42)
 
-    train_df, eval_df = train_test_split(
-        df, test_size=config.get('test_size')
-    )
+def preprocess_df(df, drop_social_pressure, drop_dups):
+    
+    if drop_social_pressure:
+        df = df.drop(columns=['6-SOCIAL', '7-PRESSURE'])
+    
+    if drop_dups:
+        df = df.drop_duplicates()
+    
+    return df
+
+def import_data(config:dict):
+
+    training_df, testing_df = load_csvs(
+        config.get('path_to_training'), 
+        config.get('path_to_testing'), 
+        config.get('concat'), 
+        config.get('test_size')
+        )
+
+    training_df = preprocess_df(training_df, config.get('drop_social_pressure'), config.get('drop_dups'))
+    testing_df = preprocess_df(testing_df, config.get('drop_social_pressure'), config.get('drop_dups'))
+    
+    labels = training_df.columns[1:].tolist()
 
     if config.get('verbose'):
-        print(f'Num Training {len(train_df)}')
-        print(f'Num Evaluation {len(eval_df)}')
+        print(f'Num Training {len(training_df)}')
+        print(f'Num Evaluation {len(testing_df)}')
 
-    return train_df, eval_df
-
-
-def gen_sample_weights(training_dataset):
-    labels = [torch.argmax(label['labels']).item() for label in
-              training_dataset]  # Assuming your dataset returns (input_vector, label) pairs
-    class_distribution = Counter(labels)
-
-    # Calculate weights for each sample
-    class_weights = {class_label: len(training_dataset) / (len(class_distribution) * class_count) for
-                     class_label, class_count in class_distribution.items()}
-    weights = [class_weights[label] for label in labels]
-
-    # Convert weights to tensor
-    weights_tensor = torch.tensor(weights, dtype=torch.float)
-
-    # Create a sampler to balance the dataset during training
-    sampler = WeightedRandomSampler(weights_tensor, len(weights_tensor))
-    return sampler
-
-
-def load_dataframes(config: dict, concat=None):
-    train_df, eval_df = load_data(config)
-    return pd.concat([train_df, eval_df])
-
-
-def load_dataset(df:pd.DataFrame, config: dict):
-
-    dataset = PersuasionDataset(
-        df,
-        config.get('model')[config.get('model_setup')]['embeddings'],
-        config
-    )
-
-    return dataset
-
-
-def gen_dataloader(df, config, train=None):
-    """
-    docstring for gen_dataloader
-
-    :param df:
-    :param config:
-    :param train:
-
-    """
-
-    print('Loading data')
-    dataset = load_dataset(df, config)
-
-    if config.get('return_dataset') and train:
-        output(dataset, config.get('training_dataset_path'))
-    elif config.get('return_dataset'):
-        output(dataset, config.get('testing_dataset_path'))
-
-    if train:
-        return torch.utils.data.DataLoader(
-            dataset,
-            batch_size=config.get('hyperparameters').get('batch_size'),
-            sampler=gen_sample_weights(dataset)
-        )
-
-    else:
-        return torch.utils.data.DataLoader(
-            dataset,
-            batch_size=1,
-            shuffle=True
-        )
-
-
-def preprocess(config: dict):
-
-    df = load_dataframes(config, concat=True)
-    train_df, eval_df = train_test_split(df, test_size=config.get('test_size'))
-    training_dataloader = gen_dataloader(train_df, config, train=True)
-    testing_dataloader = gen_dataloader(eval_df, config)
-    return training_dataloader, testing_dataloader
-
+    return training_df, testing_df, labels
 
 if __name__ == '__main__':
-    args = getargs()
-    config_ob = load_config(args.config_path)
-    preprocess(config_ob)
+    config = {
+        'path_to_training': 'training.csv',
+        'path_to_testing': 'testing.csv',
+        'DROP_SOCIAL_PRESSURE': True,
+        'concat': True,
+        'drop_dups': True,
+        'test_size': 0.2,
+        'verbose': True
+    }
+
+    train_df, test_df, label_columns = import_data(config)

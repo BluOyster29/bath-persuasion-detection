a0d359cbbd97fcd91bdb953c5ca07684de6f4fdd
BluOyster29 <rthomas2907@gmail.com>
2024-03-11 09:07:17 +0000

    added RNN - doesn't work that well

--- models/bert_classifier.py
+++ models/bert_classifier.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from transformers import BertModel
 
-# Define your classification model
+
 class BertClassifier(nn.Module):
     """
     BERT-based classifier model.
@@ -19,6 +19,7 @@ class BertClassifier(nn.Module):
 
     def __init__(self, bert_model, num_labels):
         super(BertClassifier, self).__init__()
+        self.name = 'bert'
         self.bert = BertModel.from_pretrained(bert_model)
         self.dropout = nn.Dropout(0.1)
         self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
@@ -40,4 +41,40 @@ class BertClassifier(nn.Module):
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         return logits
+
+
+class RNN(nn.Module):
+    def __init__(self, vocab_size, config, output_size):
+        super(RNN, self).__init__()
+        self.name = 'rnn'
+        self.embedding_size = config.get('embedding_size')
+        self.hidden_size = config.get('hidden_size')
+        self.embd = nn.Embedding(
+            vocab_size, self.embedding_size, padding_idx=0)
+        self.rnn = nn.GRU(
+            self.embedding_size, self.hidden_size, batch_first=True, 
+                num_layers=2, dropout=0.1, bidirectional=True)
+        self.dropout = nn.Dropout(0.1)
+        self.fc = nn.Linear(self.hidden_size*2, output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # x = self.embd(x)  # Convert word indices to word embeddings
+        out, _ = self.rnn(x)
+        out = self.dropout(self.relu(self.fc(out[:, -1, :])))
+        return out
+
+class FeedForward(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.1)
     
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out

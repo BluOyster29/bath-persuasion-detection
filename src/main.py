b99d82158c282f371510fb0e0fe48297259ba17f
BluOyster29 <rthomas2907@gmail.com>
2024-03-11 09:07:51 +0000

    expanded main script

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import logging 
+import sys 
 from preprocess import import_data
 from build_dataloaders import build_dataloaders
 from train import train
+from evaluate import evaluate
 from utils import get_args, load_config
 import warnings 
 
+sys.path.append('../models')
 warnings.filterwarnings("ignore", category=FutureWarning) 
 # until i work out pandas bug 
 
@@ -12,21 +15,36 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def fetch_data(config):
+    """
+    Fetch the data from the specified data source.
+
+    Args:
+        config (dict): The configuration dictionary.
+
+    Returns:
+        pd.DataFrame: The training data.
+        pd.DataFrame: The testing data.
+    """
+
+    logger.info('Fetching Data')
+    train_df, test_df, vocab = import_data(config)
+    logger.info('Data Fetched')
+    return train_df, test_df, vocab
+
+
 def main(parsed_args):
-    
-    logger.info('Loading config from %s', parsed_args.config_path)
+
     config = load_config(parsed_args.config_path)
-    logger.info('Config Loaded')
-    logger.info('Importing Data')
-    train_df, test_df, label_columns = import_data(config)
-    logger.info('Data Imported')
-    training_dataloader, testing_dataloader = build_dataloaders(train_df[:1], test_df[:1], config)
-    logger.info('Data Loaded')
-    trained_model = train(config, label_columns, training_dataloader)
-    logger.info('Training Complete')
-    
-    logger.info('Saving Model')
-    
+    train_df, test_df, vocab = fetch_data(config)
+
+    training_dataloader, testing_dataloader = build_dataloaders(
+        train_df, test_df, config, vocab)
+
+    trained_model = train(config, training_dataloader)
+
+    evaluate(config, testing_dataloader, trained_model)
+
 
 if __name__ == '__main__':
     logger.info('Starting Main')

a9e22e75bcfda150597c9f263e586bbed454658a
BluOyster29 <rthomas2907@gmail.com>
2024-03-11 14:36:09 +0000

    formatting

--- src/train.py
+++ src/train.py
@@ -1,25 +1,31 @@
 import sys
-sys.path.append('/Users/rt853/repos/UoB/bath-persuasion-detection/models')
-
-from tqdm.auto import tqdm
-from bert_classifier import BertClassifier, RNN
-import torch.nn as nn
 import torch
+import os
+import torch.nn as nn
+from tqdm.auto import tqdm
 
-from utils import gen_metadata
+module_dir = os.path.expanduser('~/repos/UoB/bath-persuasion-detection/models')
+sys.path.append(module_dir)
+
+from bert_classifier import BertClassifier, RNN # noqa
+from utils import gen_metadata # noqa
 
 
-def init_hyperparameeters(config, label_columns,vocab_size=None):
+def init_hyperparameeters(config, label_columns, vocab_size=None):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     num_epochs = config.get('num_epochs')
-    criterion = nn.BCEWithLogitsLoss()  
+    criterion = nn.BCEWithLogitsLoss()
+
     if config.get('use_pretrained'):
-        model = BertClassifier(config.get('pretrained_model'), len(label_columns))
+        model = BertClassifier(config.get('pretrained_model'),
+                               len(label_columns))
     else:
         model = RNN(vocab_size, config, len(label_columns))
+
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-5, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=2e-5, weight_decay=1e-5)
     return device, num_epochs, criterion, model, optimizer
 
 
@@ -44,7 +50,7 @@ def train_model(
                         labels = batch['labels'].to(device)
                         embeddings = model.embd(input_ids)
                         outputs = model(embeddings)
- 
+
                     elif model.name == 'bert':
                         input_ids = batch['input_ids'].to(device)
                         attention_mask = batch['attention_mask'].to(device)
@@ -56,12 +62,16 @@ def train_model(
                     loss.backward()
                     optimizer.step()
 
-                    t2.set_description(
-                        f'Epoch: {e} | Batch {b} | Average Loss: {round(sum(epoch_loss)/len(epoch_loss),4)}')
+                    avg_epoch_loss = round(sum(epoch_loss)/len(epoch_loss), 4)
+                    description = f'Epoch: {e} | '
+                    description += f'Batch {b} | '
+                    description += f'Average Loss: {avg_epoch_loss}'
+
+                    t2.set_description(description)
                     t2.update()
 
         t.set_description(
-        f'Average Epoch Loss: {round(sum(epoch_loss)/len(epoch_loss),4)}')
+            f'Average Epoch Loss: {round(sum(epoch_loss)/len(epoch_loss),4)}')
         t.update()
 
     return avg_loss, model
@@ -69,25 +79,31 @@ def train_model(
 
 def train(config, training_dataloader):
     """
-    Trains a model using the provided configuration, label columns, and training dataloader.
+    Trains a model using the provided configuration, label columns, and
+    training dataloader.
 
     Args:
         config (dict): Configuration parameters for training.
         label_columns (list): List of column names for the labels.
-        training_dataloader (DataLoader): DataLoader object containing the training data.
+        training_dataloader (DataLoader): DataLoader object containing
+        the training data.
 
     Returns:
         tuple: A tuple containing the average loss and the trained model.
     """
-    vocab_size=None
+
+    vocab_size = None
     label_columns = training_dataloader.dataset.label_columns
     if not config.get('use_pretrained'):
         vocab_size = training_dataloader.dataset.vocab_size
-    device, num_epochs, criterion, model, optimizer = init_hyperparameeters(config, label_columns, vocab_size)
-    avg_loss, trained_model = train_model(model, training_dataloader, num_epochs, device, optimizer, criterion)
+    device, num_epochs, criterion, model, optimizer = init_hyperparameeters(
+        config, label_columns, vocab_size)
+    avg_loss, trained_model = train_model(
+        model, training_dataloader, num_epochs, device, optimizer, criterion)
     meta_data = gen_metadata(config, 'model')
-    
-    if config.get('output_model'): 
-        torch.save(trained_model.state_dict(), f"{config.get('output_model')}_{meta_data}_{avg_loss}.pth")
-        
+
+    if config.get('output_model'):
+        torch.save(trained_model.state_dict(),
+                   f"{config.get('output_model')}_{meta_data}_{avg_loss}.pth")
+
     return trained_model

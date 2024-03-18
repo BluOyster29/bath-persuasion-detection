a169b3e00c5571075f6662691f7d028de606fa54
BluOyster29 <rthomas2907@gmail.com>
2024-03-18 14:16:01 +0000

    moved files

---
+++ src/data_processing/process_seq_task.py
@@ -0,0 +1,119 @@
+import pandas as pd 
+import json 
+import re
+import argparse
+
+p = argparse.ArgumentParser()
+p.add_argument('jsonl_path', type=str, help='Path to the jsonl file')
+p.add_argument('output_path', type=str, help='Path to the output file')
+args = p.parse_args()
+
+label_columns = [
+    '1-RAPPORT',
+    '2-NEGOTIATE',
+    '3-EMOTION',
+    '4-LOGIC',
+    '5-AUTHORITY',
+    '6-SOCIAL',
+    '7-PRESSURE',
+    '8-NO-PERSUASION'
+]
+
+def open_jsonl(file_path):
+    with open(file_path, 'r') as f:
+        data = f.readlines()
+    return data
+
+
+def gen_idx_map(text):
+
+    start_idx = 0
+    idx_map = {}
+    
+    for idx, i in enumerate(text.split('\n')):
+        
+        end_idx = start_idx + len(i)
+        idx_map[idx] = (start_idx, end_idx)
+        start_idx = end_idx
+    return idx_map
+
+
+def add_labels_to_text(text, labels):
+
+    idx_map = gen_idx_map(text)
+    splat_text = text.split('\n')
+    for label in labels:
+        _ ,end_idx ,label = label
+        for idx, (s, e) in idx_map.items():
+            if end_idx <= e:
+                splat_text[idx] += ' - ' + label
+                break
+    return splat_text 
+
+
+def process_transcripts(jsonl):
+
+    transcripts = []
+    for idx, line in enumerate(jsonl):
+        j = json.loads(line)
+        text = j['text']
+        labels = j['label']
+        if len(labels) < 1:
+            break
+        transcripts.append(add_labels_to_text(text, labels))
+        
+    return transcripts
+
+
+def process_line(line):
+
+    split_line = line.split(' - ')
+    
+    if len(split_line) > 2:
+        text, label = split_line[0], split_line[1:]
+        
+    elif len(split_line) < 2:
+        text, label = line, '8-NO-PERSUASION'
+    else:
+        text, label = line.split(' - ')
+
+
+    return text[10:], label
+
+
+def update_labels(label_columns, data, labels:list):
+    if not isinstance(labels, list):
+        labels = [labels]
+        
+    for i in label_columns:
+        if i in labels:
+            data[i].append(1)
+        else:
+            data[i].append(0)
+    return data
+    
+def generate_dataframe(jsonl, label_columns):
+
+    transcripts = process_transcripts(jsonl)
+    
+    data = {i : [] for i in label_columns}
+    data['text'] = []
+    
+    for t in transcripts:
+        for line in t:
+            
+            if re.match('Persuader:', line):
+                text, label = process_line(line)
+                data['text'].append(text)
+                data = update_labels(label_columns, data, label)
+                
+            else:
+                continue
+    df = pd.DataFrame(data)
+    return df[['text'] + label_columns]
+
+
+if __name__ == '__main__':
+    jsonl = open_jsonl(args.jsonl_path)
+    df = generate_dataframe(jsonl, label_columns)
+    df.to_csv(args.output_path, index=False)

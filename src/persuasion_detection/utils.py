e4e3921885fbb0b3ef533b8638f8f2b50e306e24
BluOyster29 <rthomas2907@gmail.com>
2024-03-27 17:35:06 +0000

    udpated

--- src/persuasion_detection/utils.py
+++ src/persuasion_detection/utils.py
@@ -1,11 +1,19 @@
-from torch import load
+import torch
 import os
 import argparse
 import yaml
-import pickle
 import logging
 import sys
 import time
+import dill as pickle
+import pandas as pd 
+from transformers import AutoTokenizer
+import sys 
+
+sys.path.append('/Users/rt853/repos/UoB/bath-persuasion-detection/src/persuasion_detection')
+
+from persuasion_strategy_dataset import PersuasionStrategyDataset
+from torch.utils.data import DataLoader
 
 module_dir = os.path.expanduser(
     '~/repos/UoB/bath-persuasion-detection/models/')
@@ -60,18 +68,53 @@ def gen_metadata(config, metatype, **kwargs):
 
 
 def load_model(config, num_classes):
-
-    model = BertClassifier(config.get('pretrained_model'), num_classes)
-    model.load_state_dict(load(config.get('model_path')))
+    
+    model = BertClassifier(
+        'bert',
+        config.get('pretrained_model'),
+        
+        num_classes)
+    
+    print('Loading model from: ', config.get('testing_model_path'))
+    
+    model.load_state_dict(
+        torch.load(
+            config.get('testing_model_path'),
+            map_location=torch.device('cpu')
+        )
+    )
+    
+    model.eval()
     return model
 
 
-def load_dataloader(config, item_name):
-    with open(
-        f"{config.get('eval_dataloader_path')}.pkl",
-        'rb'
-            ) as f:
-        dataloader = pickle.load(f)
+def load_dataloader(config):
+
+    if config.get('eval_dataloader_path')[-4:] == '.pkl':
+        with open(
+            f"{config.get('eval_dataloader_path')}",
+            'rb'
+                ) as f:
+            dataloader = pickle.load(f)
+    
+    elif config.get('eval_dataloader_path')[-4:] == '.csv':
+        df = pd.read_csv(config.get('eval_dataloader_path'))
+        
+        if config.get('drop_social_pressure'):
+            
+            df = df.drop(columns=['6-SOCIAL', '7-PRESSURE'])
+            
+        pdataset = PersuasionStrategyDataset(
+            df,
+            AutoTokenizer.from_pretrained(config.get('pretrained_model')),
+            max_token_len=config.get('max_token_len')
+        )
+        
+        dataloader = DataLoader(
+            pdataset,
+            batch_size=32,
+        )
+
     return dataloader
 
 
